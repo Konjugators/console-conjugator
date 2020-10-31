e6078aa7b1da49349c8ea523b5c46ca594f951e2
shua1090 <shynn.lawrence@gmail.com>
2020-10-30 20:46:39 -0700

    added imperativ

--- conjugator.py
+++ conjugator.py
@@ -1,7 +1,7 @@
-import csv
 
 '''Things to do when the file is imported'''
 if __name__ != "__main__":
+    import csv
     global tohave
     tohave = ["habe", 'hast', 'hat', 'haben', 'habt', 'breaker',
             'hatte', 'hattest', 'hatte', 'hatten', 'hattet', 
@@ -63,26 +63,27 @@ def presentconjugate(verb, pronoun):
     verblist = conjugations[verbnum]
 
 
-def conjugate(verb:str, pronoun="Sie", tense="present"):
+def conjugate(verb:str, pronoun="alles", tense="present"):
     assert "*" not in verb
     if tense == "present":
         return presentconjugate(verb, pronoun)
     if tense == "present perfect":
         return presentperfectconjugate(verb, pronoun)
+    if tense == "imperative":
+        return imperativeconjugate(verb, pronoun)
 
-def presentperfectconjugate(verb:str, pronoun):
+def presentperfectconjugate(verb:str, pronoun:str):
     verbnum = infinitives.index(verb)
     if conjugations[verbnum][9] == "haben":
         hilfensverb = tohave
     elif conjugations[verbnum][9] == "sein":
         hilfensverb = tobe
     else:
-        return 0
-
+        return 1
     if pronoun == "er" or pronoun == "es":
         hilfensverb = hilfensverb[2]
-    if pronoun == "sie":
-        hilfesnverb = hilfensverb[2] + "/" + hilfensverb[3]
+    if pronoun.lower() == "sie":
+        hilfensverb = hilfensverb[2] + "/" + hilfensverb[3]
     if pronoun == "ich":
         hilfensverb = hilfensverb[0]
     if pronoun == "du":
@@ -90,3 +91,12 @@ def presentperfectconjugate(verb:str, pronoun):
     if pronoun == "ihr":
         hilfensverb = hilfensverb[4]
     return hilfensverb + " " + conjugations[verbnum][5]
+
+def imperativeconjugate(verb:str, pronoun:str):
+    verbnum = infinitives.index(verb)
+    if pronoun == "du":
+        return conjugations[verbnum][7]
+    if pronoun == "ihr":
+        return conjugations[verbnum][8]
+    if pronoun == "sie" or pronoun == "wir":
+        return conjugations[verbnum][7]+"en"
