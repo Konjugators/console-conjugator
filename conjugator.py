80bc0b6a0fe2cbe37706db765a30aecf1ae02ae1
Govind Gnanakumar <govind.gnanakumar@outlook.com>
2020-10-30 21:10:41 -0700

    Minor changes

--- conjugator.py
+++ conjugator.py
@@ -1,48 +1,76 @@
-
-'''Things to do when the file is imported'''
+"""Things to do when the file is imported"""
 if __name__ != "__main__":
     import csv
+
     global tohave
-    tohave = ["habe", 'hast', 'hat', 'haben', 'habt', 'breaker',
-            'hatte', 'hattest', 'hatte', 'hatten', 'hattet', 
-            'breaker', 'gehabt']
-    tobe = ["bin", "bist", "ist", "sind", "seid", "breaker",
-            "war", "warst", "war", "waren", "wart", 
-            'breaker', 'gewesen']
+    tohave = [
+        "habe",
+        "hast",
+        "hat",
+        "haben",
+        "habt",
+        "breaker",
+        "hatte",
+        "hattest",
+        "hatte",
+        "hatten",
+        "hattet",
+        "breaker",
+        "gehabt",
+    ]
+    tobe = [
+        "bin",
+        "bist",
+        "ist",
+        "sind",
+        "seid",
+        "breaker",
+        "war",
+        "warst",
+        "war",
+        "waren",
+        "wart",
+        "breaker",
+        "gewesen",
+    ]
 
     global conjugations, infinitives, indexDict
-    #Conjugations as 2d List of all conjugations/tenses
-    #Infinitives as just the infinitive forms of all verbs -> Faster indexing
+    # Conjugations as 2d List of all conjugations/tenses
+    # Infinitives as just the infinitive forms of all verbs -> Faster indexing
     conjugations, infinitives = [], []
-    #Dictionary of indexes to tenses/conjugations
-    with open('verbs.csv', 'r', newline="") as file:
+    # Dictionary of indexes to tenses/conjugations
+    with open("verbs.csv", "r", newline="") as file:
         verblist = csv.reader(file)
         for row in verblist:
             conjugations.append(row)
             infinitives.append(row[0])
 
-def findIndex(verb:str):
+
+def findIndex(verb: str):
     assert len(verb) > 0
     return infinitives.index(verb)
 
-def line(row:int):
+
+def line(row: int):
     assert row != -1
     return conjugations[row]
 
-def deZuEn(word:str):
+
+def deZuEn(word: str):
     z = 0
     val = 0
     newwrt = ""
-    conv = {"a":"ä", "o":"ö", "u":"ü", "s":"ß"}
+    conv = {"a": "ä", "o": "ö", "u": "ü", "s": "ß"}
     convlist = ["a", "o", "u", "s"]
-    while (val <= len(word)-1):
-        if word[val] in convlist and word[val+1] == "*":
+    while val <= len(word) - 1:
+        if word[val] in convlist and word[val + 1] == "*":
             newwrt += conv[word[val]]
             val += 2
         else:
             newwrt += word[val]
             val += 1
     return newwrt
+
 
 def presentconjugate(verb, pronoun):
     verbnum = infinitives.index(verb)
@@ -51,19 +79,24 @@ def presentconjugate(verb, pronoun):
             if conjugations[verbnum][0][-2] == "e":
                 return conjugations[verbnum][0][:-2] + "t"
             else:
-                return conjugations[verbnum][0][:-1]+"t"
+                return conjugations[verbnum][0][:-1] + "t"
     if pronoun.lower() == "ich":
         return conjugations[verbnum][1]
     if pronoun.lower() == "du":
         return conjugations[verbnum][2]
     if pronoun.lower() == "sie":
-        return conjugations[verbnum][3] + " for \"she\" and " + conjugations[verbnum][0] + " for plural and formal You"
+        return (
+            conjugations[verbnum][3]
+            + ' for "she" and '
+            + conjugations[verbnum][0]
+            + " for plural and formal You"
+        )
     if pronoun.lower() == "er" or pronoun.lower() == "es":
         return conjugations[verbnum][3]
     verblist = conjugations[verbnum]
 
 
-def conjugate(verb:str, pronoun="alles", tense = "present"):
+def conjugate(verb: str, pronoun="alles", tense="present"):
     assert "*" not in verb
     if tense == "present":
         return presentconjugate(verb, pronoun)
@@ -74,7 +107,8 @@ def conjugate(verb:str, pronoun="alles", tense = "present"):
     if tense == "partizip 1":
         return partizip1conjugate(verb)
 
-def presentperfectconjugate(verb:str, pronoun:str):
+
+def presentperfectconjugate(verb: str, pronoun: str):
     verbnum = infinitives.index(verb)
     if conjugations[verbnum][9] == "haben":
         hilfensverb = tohave
@@ -94,16 +128,17 @@ def presentperfectconjugate(verb:str, pronoun:str):
         hilfensverb = hilfensverb[4]
     return hilfensverb + " " + conjugations[verbnum][5]
 
-def imperativeconjugate(verb:str, pronoun:str):
+
+def imperativeconjugate(verb: str, pronoun: str):
     verbnum = infinitives.index(verb)
     if pronoun == "du":
         return conjugations[verbnum][7]
     if pronoun == "ihr":
         return conjugations[verbnum][8]
     if pronoun == "sie" or pronoun == "wir":
-        return conjugations[verbnum][7]+"en"
+        return conjugations[verbnum][7] + "en"
 
-def partizip1conjugate(verb:str):
+
+def partizip1conjugate(verb: str):
     verbnum = infinitives.index(verb)
     return conjugations[verbnum][0] + "d"
-
