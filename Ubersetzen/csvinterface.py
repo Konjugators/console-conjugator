21d4fd965cd0541a162a8810c5d2be339a0313d1
shynn <shynn.lawrence@gmail.com>
2020-11-13 19:52:59 -0800

    Preparing for translating

--- Ubersetzen/csvinterface.py
+++ Ubersetzen/csvinterface.py
@@ -1,9 +1,11 @@
-def appverbs(z:list):
-    with open("Ubersetzen/dict.csv", "a") as newverbs:
+def appverbs(z: list):
+    with open("Ubersetzen/DeutschDictionary.csv", "a") as newverbs:
         csvWriter = csv.writer(newverbs, delimiter=",")
         csvWriter.writerows(z)
+
 
 if __name__ == "__main__" or __name__ != "__main__":
     import csv
     import os
+
     this_dir, this_filename = os.path.split(__file__)
--- Ubersetzen/dictionary_maker.py
+++ Ubersetzen/dictionary_maker.py
@@ -1,13 +1,23 @@
 my_file = open("Ubersetzen/german.txt", "r", encoding="utf-8")
 content = my_file.read()
-content = content[0:1000].split("\n")
+content = content.split("\n")
+
 
 import csvinterface
 from googletrans import Translator
+
 transmaschine = Translator()
+x = 0  # Progress update
+z = lambda a: transmaschine.translate(a, dest="en", src="de")
 for val in content:
     try:
-        val2 = transmaschine.translate(val, dest="en")
-        csvinterface.appverbs([[val, val2.text]])
+        temp = z(val).text
+        if temp != val:
+            csvinterface.appverbs([[val, temp]])
+        else:
+            pass
     except:
         pass
+    if x % 100 == 0:
+        print("Currently at index: " + str(x))
+    x += 1
