5776caa7fc7c60e2e2621cb1735c602cd0704d00
shynn <shynn.lawrence@gmail.com>
2020-11-23 18:16:57 -0800

    Setup Francais conjugation folder

---
+++ Francaisconjugation/cli_scripts.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+# Parse Args
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mode', nargs= 1)
+    parser.add_argument("i", nargs=("?" if 'f' in sys.argv else 3), help="The unconjugated verb")
+
+    args = parser.parse_args()
+    return args
+
+
+# Change words in german to english, so that the conjugation process works properly
+def tensePreprocessing(tense: str) -> str:
+    if tense in ["präsens"]:
+        return "present"
+    if tense in ["simplepast", "präteritum", "prateritum"]:
+        return "simple-past"
+    if tense in ["presentperfect", "perfekt"]:
+        return "present-perfect"
+    if tense in ["plusquamperfect", "pastperfect", "pastPerfect"]:
+        return "past-perfect"
+    if tense in ["zukunft"]:
+        return "future"
+    else:
+        return tense
+
+# Literally translates to "Mode understanding - c and f are the mode arguments"
+def modeParse():
+    args = get_args()
+    if args.mode[0] == "f":
+        from Deutschconjugation import fuzzy
+        fuzzy.start()
+    if args.mode[0] == "c":
+        from Deutschconjugation import conjugator as conj
+        infinitive, pronoun, tense = lower_format()
+        tense = tensePreprocessing(tense)
+        z = conj.conjugate(infinitive, pronoun, tense)
+        print(z)
+
+# Lower_case the args
+def lower_format():
+    args = get_args()
+    if len(args.i) < 3:
+        raise("You are missing a few things")
+        print("error")
+    if len(args.i) > 3:
+        raise("You have too many things")
+    return args.i[0].lower(), args.i[1].lower(), args.i[2].lower()
+    # infinitive.lower(), args.pronoun, args.tense.lower()
+
+# Conjugate and print args
+def main():
+    modeParse()
+
+if __name__ != "__main__":
+    import argparse
+
+if __name__ == "__main__":
+    import argparse
+
+# TODO Table
---
+++ Francaisconjugation/conjugator.py
@@ -0,0 +1,3 @@
+# TODO: CSV load when imported (__main__ != "__main__")
+# TODO: formatting -> Convert c^ to ç and so on
+# TODO: Function that takes in 3 arguments and returns conjugated verb
