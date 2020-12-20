09648b02a078ec1c0f8a1409bc0a68c60d5ea7e7
AdiMatam <66976288+AdiMatam@users.noreply.github.com>
2020-12-20 09:13:03 -0800

    Update cli_scripts.py
    minor change to color function

--- Francaisconjugation/cli_scripts.py
+++ Francaisconjugation/cli_scripts.py
@@ -3,9 +3,7 @@ import platform
 from Francaisconjugation import conjugator
 
 def getColorAvailability() -> bool:
-    if platform.system() not in ["Linux", "Darwin"]: colors = False
-    else: colors = True
-    return colors
+    return platform.system() in ("Linux", "Darwin")
 
 def get_args() -> str:
     parser = argparse.ArgumentParser()
